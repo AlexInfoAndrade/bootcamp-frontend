0b25825891e4e1f83ad97d94d624e22391187183
Alexander Santos Andrade <al.praia@gmail.com>
2021-05-29 23:43:18 -0300

    Alterando baseUrl para https

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -7,7 +7,7 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
-  readonly baseUrl = 'http://bootcamp-dio-alex.herokuapp.com/bootcamp';
+  readonly baseUrl = 'https://bootcamp-dio-alex.herokuapp.com/bootcamp';
 
   constructor(private http: HttpClient) { }
 
